6fde3c8534a6aad81d914efb2c168df0bb4711ab
index506 <54052375+index506@users.noreply.github.com>
2019-09-22 17:08:42 +0900

    프로젝트 파일

---
+++ 3rd_Project/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("#main_gnb .gnb .gnb_list h2 a").mouseenter(function(){
+        var index = $(this).parent().parent().index();
+        $("#main_gnb .gnb_"+index+" .menu").addClass("hover");
+        $("#main_gnb .gnb_list").not(".gnb_"+index).children(".menu").removeClass("hover");
+    });
+    $("#main_header_wrap").mouseleave(function(){
+        $("#main_gnb .menu").removeClass("hover");
+    });
+});
