a95e2b93c74d14d60065f812464df85344bb7adb
DanielCraig777 <54057557+DanielCraig777@users.noreply.github.com>
2019-09-25 19:01:16 +0900

    석인영

--- 3rd_Project/js/common.js
+++ 3rd_Project/js/common.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $("#main_gnb .gnb .gnb_list h2 a").mouseenter(function(){
         var index = $(this).parent().parent().index();
         $("#main_gnb .gnb_"+index+" .menu").addClass("hover");
@@ -7,5 +6,11 @@ $(document).ready(function(){
     });
     $("#main_header_wrap").mouseleave(function(){
         $("#main_gnb .menu").removeClass("hover");
+    });
+    $(".alert_ready").click(function(){
+        alert("준비중입니다.");
+    });
+    $("#top_menu .loginOnOff").click(function(){
+        $("#top_menu ul").toggleClass("on");
     });
 });
