926f5dec693657f3090b1bd4ee8e41abe5419d79
dksduddn93 <54062325+dksduddn93@users.noreply.github.com>
2019-09-25 10:17:54 +0900

    최신 문서, 프로젝트 업로드
    ...

--- 3rd_Project/Member/An/js/testTake.js
+++ 3rd_Project/Member/An/js/testTake.js
@@ -1,4 +1,28 @@
+
 $(document).ready(function(){
+    // start of 문제 생성
+    for(var i=1; i<=LAST_NUM; i++){
+        var txt='';
+        txt+='<div id="quiz_box_'+i+'" class="quiz_box">';
+        txt+='<ul>';
+        txt+='<li><div>['+i+'번문제]</div><div>'+quiz[i-1][0]+'</div></li>';
+        if(quiz[i-1][1]!=null){
+            txt+='<li><img src="'+quiz[i-1][1]+'"></li>';
+        }
+        for(j=1; j<=4; j++){
+            txt+='<li>';
+            txt+='<input id="answer'+i+'_'+j+'" class="answer" type="radio" name="'+i+'" value="'+i+'"/>';
+            txt+='<label for="answer'+i+'_'+j+'">'+quiz[i-1][j+1]+'</label>';
+            txt+='</li>';
+        }
+        txt+='</ul>';
+        txt+='</div>';   
+        $('#content_area').prepend(txt);
+    }
+
+    $('#quiz_box_1').addClass('now');
+    // end of 문제 생성
+    
 
     // start of 남은 시간
     const testTimeMin = 90; // 분단위로 DB에서 시험시간 받아오기
@@ -9,13 +33,11 @@ $(document).ready(function(){
     timeTextWrite(hour,min,sec);
 
     var timer=setInterval(function(){
-        if(--sec<0) {
-            sec=59;
-            if(--min<0){
-                min=59;
+        if(--sec<0) { sec=59;
+            if(--min<0){ min=59;
                 if(--hour<0){
                     alert("시험이 종료되었습니다.");
-                    $('#save_sub').trigger("click");
+                    $('#save.btn').trigger("click");
                     //location.href="./testResult.html";
                 } // end of hour
             } // end of min
@@ -24,19 +46,20 @@ $(document).ready(function(){
     }, 1000);
 
     function timeTextWrite(hour, min, sec){
-        $('span.time').text(zeroConcat(hour)+":"+zeroConcat(min)+":"+zeroConcat(sec)); // time td의 내용 교체
+        $('.time').text(zeroConcat(hour)+":"+zeroConcat(min)+":"+zeroConcat(sec)); // time td의 내용 교체
     }
+
     function zeroConcat(time){
         if(time<10) time="0"+time; // 0~9는 앞에 0 붙임 (00,09)
         return time;
     }
     //  end of 남은시간
 
-    // start of 푼 문제 번호이동버튼 색칠
+    // start of 풀지 않은 문제, 푼 문제 색칠 기능
     $('.answer').click(function(){
         var quizNum = $(this).attr('name');
         $('#gbtn'+quizNum).addClass('is');
     });
-    // end of 푼 문제 번호이동버튼 색칠
+    // end of 풀지 않은 문제, 푼 문제 색칠 기능
 
 });
