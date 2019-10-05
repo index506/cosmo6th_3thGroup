04504016824bfaeb3b8113c5bf5be1f42b4d566d
dksduddn93 <54062325+dksduddn93@users.noreply.github.com>
2019-10-06 01:10:23 +0900

    .

--- 3rd_Project/Member/An/js/manual.js
+++ 3rd_Project/Member/An/js/manual.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     $('.manual_btn').click(function(){
-        var hei = $('#content_wrap').height();
-        $('.manual_bg').height(hei).fadeToggle('fast');
         $('.manual_item').toggleClass('manual_show');
     });
     
