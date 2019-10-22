5e5ed5b5722a50d2b1ed07c97f9ee6963526e486
dksduddn93 <54062325+dksduddn93@users.noreply.github.com>
2019-10-22 19:08:35 +0900

    DB DATE COMMIT

--- JSP/An/DB/시험 데이터 DB.sql
+++ JSP/An/DB/시험 데이터 DB.sql
@@ -1,7 +1,7 @@
-/* create */
+/* create 문제 */
 create table TEST_EIP_20190427(
     test_num number(5) primary key,
-    test_title VARCHAR2(400),
+    test_title VARCHAR2(500),
     test_imageName VARCHAR2(50) default NULL,    
     test_1 VARCHAR2(250), 
     test_2 VARCHAR2(250), 
@@ -12,7 +12,7 @@ create table TEST_EIP_20190427(
 
 create table TEST_EIP_20190303(
     test_num number(5) primary key,
-    test_title VARCHAR2(400),
+    test_title VARCHAR2(500),
     test_imageName VARCHAR2(50) default NULL,    
     test_1 VARCHAR2(250), 
     test_2 VARCHAR2(250), 
@@ -23,7 +23,7 @@ create table TEST_EIP_20190303(
 
 create table TEST_LM1_20190316(
     test_num number(5) primary key,
-    test_title VARCHAR2(400),
+    test_title VARCHAR2(500),
     test_imageName VARCHAR2(50) default NULL,    
     test_1 VARCHAR2(250), 
     test_2 VARCHAR2(250), 
@@ -32,6 +32,19 @@ create table TEST_LM1_20190316(
     test_subject VARCHAR2(100)
 ); /* LM1(Linux Master 1grade) */
 
+create table TEST_LM1_20190921(
+    test_num number(5) primary key,
+    test_title VARCHAR2(500),
+    test_imageName VARCHAR2(50) default NULL,    
+    test_1 VARCHAR2(250), 
+    test_2 VARCHAR2(250), 
+    test_3 VARCHAR2(250), 
+    test_4 VARCHAR2(250),
+    test_subject VARCHAR2(100)
+); /* LM1(Linux Master 1grade) */
+
+
+/* create 해답 */
 create table ANSWER_EIP_20190427(
     test_num number(5) primary key,
     test_answer number(5),
@@ -49,6 +62,12 @@ create table ANSWER_EIP_20190303(
 ); /* 정보처리기사 EIP(Engineer Information Processing)*/
 
 
+/* select ALL */
+SELECT * FROM TEST_EIP_20190427;
+SELECT * FROM TEST_EIP_20190303;
+SELECT * FROM TEST_LM1_20190921;
+SELECT * FROM TEST_LM1_20190316;
+
 /* select */
 SELECT * FROM TEST_EIP_20190427 WHERE TEST_IMAGENAME IS NOT NULL;
 SELECT * FROM TEST_EIP_20190427 WHERE TEST_IMAGENAME IS NULL;
@@ -59,10 +78,14 @@ SELECT * FROM TEST_EIP_20190303 WHERE TEST_IMAGENAME IS NULL;
 SELECT * FROM ANSWER_EIP_20190427;
 SELECT * FROM ANSWER_EIP_20190303;
 
-
 /* drop */
 DROP TABLE ANSWER_EIP_20190427; 
 DROP TABLE ANSWER_EIP_20190303; 
 
 DROP TABLE TEST_EIP_20190427; 
 DROP TABLE TEST_EIP_20190303; 
+DROP TABLE TEST_LM1_20190921; 
+DROP TABLE TEST_LM1_20190316; 
+
+
+
