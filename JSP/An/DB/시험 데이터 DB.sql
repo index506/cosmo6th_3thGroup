418971aa319257fb5c1807820df99aa17406eb8f
dksduddn93 <54062325+dksduddn93@users.noreply.github.com>
2019-10-22 14:17:47 +0900

    war, DB 업로드

---
+++ JSP/An/DB/시험 데이터 DB.sql
@@ -0,0 +1,68 @@
+/* create */
+create table TEST_EIP_20190427(
+    test_num number(5) primary key,
+    test_title VARCHAR2(400),
+    test_imageName VARCHAR2(50) default NULL,    
+    test_1 VARCHAR2(250), 
+    test_2 VARCHAR2(250), 
+    test_3 VARCHAR2(250), 
+    test_4 VARCHAR2(250),
+    test_subject VARCHAR2(100)
+); /* 정보처리기사 EIP(Engineer Information Processing)*/
+
+create table TEST_EIP_20190303(
+    test_num number(5) primary key,
+    test_title VARCHAR2(400),
+    test_imageName VARCHAR2(50) default NULL,    
+    test_1 VARCHAR2(250), 
+    test_2 VARCHAR2(250), 
+    test_3 VARCHAR2(250), 
+    test_4 VARCHAR2(250),
+    test_subject VARCHAR2(100)
+); /* 정보처리기사 EIP(Engineer Information Processing) */
+
+create table TEST_LM1_20190316(
+    test_num number(5) primary key,
+    test_title VARCHAR2(400),
+    test_imageName VARCHAR2(50) default NULL,    
+    test_1 VARCHAR2(250), 
+    test_2 VARCHAR2(250), 
+    test_3 VARCHAR2(250), 
+    test_4 VARCHAR2(250),
+    test_subject VARCHAR2(100)
+); /* LM1(Linux Master 1grade) */
+
+create table ANSWER_EIP_20190427(
+    test_num number(5) primary key,
+    test_answer number(5),
+    
+    CONSTRAINT fk_ANSWER_EIP_20190427_num FOREIGN KEY(test_num)
+    REFERENCES TEST_EIP_20190427(test_num)
+); /* 정보처리기사 EIP(Engineer Information Processing)*/
+
+create table ANSWER_EIP_20190303(
+    test_num number(5) primary key,
+    test_answer number(5),
+    
+    CONSTRAINT fk_ANSWER_EIP_20190303_num FOREIGN KEY(test_num)
+    REFERENCES TEST_EIP_20190303(test_num)
+); /* 정보처리기사 EIP(Engineer Information Processing)*/
+
+
+/* select */
+SELECT * FROM TEST_EIP_20190427 WHERE TEST_IMAGENAME IS NOT NULL;
+SELECT * FROM TEST_EIP_20190427 WHERE TEST_IMAGENAME IS NULL;
+
+SELECT * FROM TEST_EIP_20190303 WHERE TEST_IMAGENAME IS NOT NULL;
+SELECT * FROM TEST_EIP_20190303 WHERE TEST_IMAGENAME IS NULL;
+
+SELECT * FROM ANSWER_EIP_20190427;
+SELECT * FROM ANSWER_EIP_20190303;
+
+
+/* drop */
+DROP TABLE ANSWER_EIP_20190427; 
+DROP TABLE ANSWER_EIP_20190303; 
+
+DROP TABLE TEST_EIP_20190427; 
+DROP TABLE TEST_EIP_20190303; 
