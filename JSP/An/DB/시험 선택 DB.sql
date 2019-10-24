01da18b5d81310c301e0925cf71efeebe928add8
dksduddn93 <54062325+dksduddn93@users.noreply.github.com>
2019-10-24 19:22:08 +0900

    update

--- JSP/An/DB/시험 선택 DB.sql
+++ JSP/An/DB/시험 선택 DB.sql
@@ -15,8 +15,9 @@ create table test_year(
 create table test_subject (
     subject_num number(10) primary key, /* (1부터 1씩 증가) */
     subject_names VARCHAR2(150), /* [데이터베이스/전자계산기 구조/운영체제/소프트웨어 공학/데이터통신] (과목별 이름) */
-    subject_question_num VARCHAR2(50), /* 20/20/20/20/20 (과목별 문제 수) */
+    subject_quiznums VARCHAR2(50), /* 20/20/20/20/20 (과목별 문제 수) */
     subject_total NUMBER(5), /* 5 (과목 수) */
+    subject_time NUMBER(10),
     subject_revision DATE, /* (시험 개정날짜) */
     license_name VARCHAR2(30),
     
@@ -37,12 +38,16 @@ insert into test_license values('리눅스마스터 1급','LM1');
 /* 기출 년도 insert <번호, 년도, 자격증 이름> */
 insert into test_year values(seq_year_num.NEXTVAL,'2019/03/03', '정보처리기사');
 insert into test_year values(seq_year_num.NEXTVAL,'2019/04/27', '정보처리기사');
+insert into test_year values(seq_year_num.NEXTVAL,'2020/03/08', '정보처리기사');
+
 insert into test_year values(seq_year_num.NEXTVAL,'2019/09/21', '리눅스마스터 1급');
 insert into test_year values(seq_year_num.NEXTVAL,'2019/03/16', '리눅스마스터 1급');
 
 /* 과목 insert 번호, 과목이름, 과목문제수, 과목개수, 과목 시험시간, 개정년도, 자격증번호*/
-insert into test_subject values(seq_subject_num.NEXTVAL,'데이터베이스/전자계산기 구조/운영체제/소프트웨어 공학/데이터통신', '20/20/20/20/20' ,5, '2000/01/01', '정보처리기사' );
-insert into test_subject values(seq_subject_num.NEXTVAL,'리눅스 실무의 이해/리눅스 시스템 관리/네트워크 및 서비스의 활용', '20/40/40',3, '2000/01/01', '리눅스마스터 1급');
+insert into test_subject values(seq_subject_num.NEXTVAL,'데이터베이스/전자계산기 구조/운영체제/소프트웨어 공학/데이터통신', '20/20/20/20/20' ,5, 150, '2000/01/01', '정보처리기사' );
+insert into test_subject values(seq_subject_num.NEXTVAL,'소프트웨어 설계/소프트웨어 개발/데이터베이스 구축/프로그래밍 언어 활용/정보시스템 구축관리', '20/20/20/20/20' ,5, 150, '2020/01/01', '정보처리기사' );
+insert into test_subject values(seq_subject_num.NEXTVAL,'리눅스 실무의 이해/리눅스 시스템 관리/네트워크 및 서비스의 활용', '20/40/40',3, 100, '2000/01/01', '리눅스마스터 1급');
+
 
 
 /* select ALL */
@@ -55,8 +60,20 @@ SELECT license_name, license_abbreviation FROM test_license
 
 /* select year */
 SELECT year_date FROM test_year WHERE license_name='정보처리기사';
-
 SELECT year_date FROM test_year WHERE license_name in ('정보처리기사')
+
+/* select subject */
+select * from test_subject where license_name='정보처리기사' AND subject_revision in 
+     (SELECT MAX(subject_revision) FROM test_subject 
+     WHERE license_name='정보처리기사' AND subject_revision < '2019/03/03');   
+
+SELECT subject_names, subject_quiznums, subject_total, subject_time 
+FROM test_subject WHERE license_name='정보처리기사' AND subject_revision IN( 
+    SELECT MAX(subject_revision) 
+    FROM test_subject 
+    WHERE license_name='정보처리기사' AND subject_revision < '2019/03/03'
+)
+
 
 /* drop YEAR - SUBJECT - LICENSE*/
 drop table test_subject;
