4dc23c9ee72de801d4561a9b3b8354e6fbd74ef8
dksduddn93 <54062325+dksduddn93@users.noreply.github.com>
2019-11-12 01:11:56 +0900

    up.

--- 3rd_Project/Category/Test/js/testExplanations.js
+++ 3rd_Project/Category/Test/js/testExplanations.js
@@ -1,50 +1,64 @@
 $(document).ready(function(){
 
-    const TOTALNUM=60;
-    var ans = new Array('W','W','W','W','W'
-                        ,'C','C','C','C','C'
-                        ,'C','W','C','C','W'
-                        ,'W','W','C','C','W'
-                        ,'C','W','C','C','W'
-                        ,'W','W','C','C','W'
-                        ,'C','W','W','C','W'
-                        ,'C','W','W','C','W'
-                        ,'C','W','C','W','W'
-                        ,'C','W','C','W','W'
-                        ,'W','W','C','W','W'
-                        ,'C','W','C','C','W');
 
-    // start of 문제 생성
-    for(var i=1; i<=LAST_NUM; i++){
+    // start of 문제 생성 1~3
+    for(var i=1; i<=3; i++){
         var txt='';
         txt+='<div id="quiz_box_'+i+'" class="quiz_box">';
         txt+='<ul>';
-        txt+='<li><div>['+i+'번문제]</div><div>'+quiz[i-1][0]+'</div></li>';
+        txt+='<li><div>'+quiz[i-1][0]+'</div></li>';
         if(quiz[i-1][1]!=null){
             txt+='<li><img src="'+quiz[i-1][1]+'"></li>';
         }
         for(j=1; j<=4; j++){
             txt+='<li>';
-            txt+='<label for="answer'+i+'_'+j+'">'+quiz[i-1][j+1]+'</label>';
+            txt+='<label id="answer'+i+'_'+j+'">'+quiz[i-1][j+1]+'</label>';
             txt+='</li>';
         }
         txt+='<div class="explanations_area">';
-        txt+='<p><span>오답률> </span><span>55%</span></p>';
-        txt+='<p><span>해설> </span><span>∀는.....스택과 큐는.....스택과 큐는.....스택과 큐는.....스택과 큐는.....</span></p>';
+        txt+='<p><span>당신의 답안 : </span><span>'+user_answer[i-1]+'</span></p>';
+        txt+='<p><span>오답률 : </span><span>'+quiz[i-1][7]+'</span></p>';
+        txt+='<p><span>[문제 해설]<br></span><span>'+quiz[i-1][8]+'</span></p>';
         txt+='</div>';               
         txt+='</ul>';
         txt+='</div>';   
-        $('#content_area').prepend(txt);
+        $('#content_area').append(txt);
+        $('#answer'+i+'_'+quiz[i-1][6]).addClass('current');
     }
-
     $('#quiz_box_1').addClass('now');
-    // end of 문제 생성                    
+    // end of 문제 생성 1~3
+    // start of 문제 생성 98~100
+    for(var i=98; i<=100; i++){
+        var txt='';
+        txt+='<div id="quiz_box_'+i+'" class="quiz_box">';
+        txt+='<ul>';
+        txt+='<li><div>'+quiz2[i-98][0]+'</div></li>';
+        if(quiz2[i-98][1]!=null){
+            txt+='<li><img src="'+quiz2[i-98][1]+'"></li>';
+        }
+        for(j=1; j<=4; j++){
+            txt+='<li>';
+            txt+='<label id="answer'+i+'_'+j+'">'+quiz2[i-98][j+1]+'</label>';
+            txt+='</li>';
+        }
+        txt+='<div class="explanations_area">';
+        txt+='<p><span>당신의 답안 : </span><span>'+user_answer2[i-98]+'</span></p>';
+        txt+='<p><span>오답률 : </span><span>'+quiz2[i-98][7]+'</span></p>';
+        txt+='<p><span>[해설]<br></span><span>'+quiz2[i-98][8]+'</span></p>';
+        txt+='</div>';               
+        txt+='</ul>';
+        txt+='</div>';   
+        $('#content_area').append(txt);
+        $('#answer'+i+'_'+quiz2[i-98][6]).addClass('current');
+    }     
+    // end of 문제 생성 98~100               
 
     // start of 맞은문제, 틀린문제 번호이동버튼
     for(var i=1; i<=TOTALNUM; i++){
         if(ans[i-1]=='C'){
-            $('#gbtn'+i).addClass('is');
-            $('#')
+            $('#gbtn'+i).addClass('current');
+        } else {
+            $('#gbtn'+i).addClass('wrong');
         }
     }
     // end of 맞은문제, 틀린문제 번호이동버튼
